39e479517a0a0b20b871f8d1b7987622af6ecc4f
SrDonEmilio <emilio@nextup.dev>
2020-05-09 17:21:43 -0500

    Fonts and webpack font manager added

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,64 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const DotEnv = require('dotenv-webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DotEnv = require("dotenv-webpack");
 
 const htmlPlugin = new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: './index.html'
-})
+  template: "./src/index.html",
+  filename: "./index.html",
+});
 
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
             },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/i,
-                use: [
-                  'file-loader',
-                  {
-                    loader: 'image-webpack-loader',
-                    options: {
-                      mozjpeg: {
-                        progressive: true,
-                        quality: 65
-                      },
-                      optipng: {
-                        enabled: false
-                      },
-                      pngquant: {
-                        quality: [0.65, 0.90],
-                        speed: 4
-                      },
-                      gifsicle: {
-                        interlaced: false
-                      },
-                      webp: {
-                        quality: 75
-                      }
-                    }
-                  }
-                ]
-              }
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
         ]
-    },
-    plugins: [htmlPlugin, new DotEnv]
-}
+      }
+    ],
+  },
+  plugins: [htmlPlugin, new DotEnv()],
+};
